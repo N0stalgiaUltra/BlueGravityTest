6a0d003c4f3be987138171eba09f2c626b47eefb
Vinicius Santos <viniciusantos0898@gmail.com>
2022-08-30 14:03:10 -0300

    Project setup; Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    float velocity = 5f;
+    float movx, movy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movx = Input.GetAxis("Horizontal");
+        movy = Input.GetAxis("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movx, movy) * velocity;
+        Rotate();
+        
+    }
+
+    void Rotate() => this.transform.rotation = movx < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+
+}
+
