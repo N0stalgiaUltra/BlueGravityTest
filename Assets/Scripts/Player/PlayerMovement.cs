a81824d8e1623964dcbec1ac988359e402c84000
Vinicius Santos <viniciusantos0898@gmail.com>
2022-09-02 11:20:44 -0300

    Finished the level design; added player walk animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-
+    [SerializeField] private Animator playerAnim;
+    
     float velocity = 5f;
     float movx, movy;
     // Update is called once per frame
@@ -18,12 +19,27 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(movx, movy) * velocity;
+        Move();
         Rotate();
         
     }
+    private void Move()
+    {
+        rb.velocity = new Vector2(movx, movy) * velocity;
 
-    void Rotate() => this.transform.rotation = movx < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+        if (Mathf.Abs(movx) > Mathf.Epsilon || Mathf.Abs(movy) > Mathf.Epsilon)
+            playerAnim.SetInteger("move", 1);
+        else
+            playerAnim.SetInteger("move", 0);
 
+    }
+    void Rotate()
+    {
+        if (movx < 0)
+            this.transform.rotation = Quaternion.Euler(0, 180, 0);
+        else if(movx > 0)
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+    }
 }
 
